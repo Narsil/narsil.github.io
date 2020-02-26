76c1805c4431880c837aa1dbce4733ee56802f1e
Narsil <Narsil@users.noreply.github.com>
2020-02-26 15:20:07 +0000

    deploy: fcf6d3a56e9a64141616aa2541152e8fd8cb335e

---
+++ assets/face/precache-manifest.f526bd34df8fb46f23bd5c002b1f6f7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f965c5540dc610abb6d9670735ec4aa9",
+    "url": "/assets/face/index.html"
+  },
+  {
+    "revision": "a7fa521130fc267ca595",
+    "url": "/assets/face/static/css/main.5ee12e06.chunk.css"
+  },
+  {
+    "revision": "fd96395832e397412b80",
+    "url": "/assets/face/static/js/2.410d8023.chunk.js"
+  },
+  {
+    "revision": "a7fa521130fc267ca595",
+    "url": "/assets/face/static/js/main.b0f5fd45.chunk.js"
+  },
+  {
+    "revision": "4fe2d31dd749a416554a",
+    "url": "/assets/face/static/js/runtime~main.3fba743e.js"
+  },
+  {
+    "revision": "c045335de26a85f7a168910ca1e5a2b6",
+    "url": "/assets/face/static/media/glass1.c045335d.png"
+  },
+  {
+    "revision": "0a0dca8af3518112124af2bce3605d5d",
+    "url": "/assets/face/static/media/glass2.0a0dca8a.png"
+  },
+  {
+    "revision": "8430f745ff9ac668662ecc84b25061e5",
+    "url": "/assets/face/static/media/glass3.8430f745.png"
+  },
+  {
+    "revision": "de67f8db166b5cc9461dcd6d582e453f",
+    "url": "/assets/face/static/media/glass5.de67f8db.png"
+  }
+]);
