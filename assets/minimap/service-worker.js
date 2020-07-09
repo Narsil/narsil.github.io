6e6a6dddeac44614a3a01be943e9eaf252c6b2d7
Nicolas Patry <patry.nicolas@protonmail.com>
2020-07-09 19:09:08 +0200

    Update homepage for minimap.

--- assets/minimap/service-worker.js
+++ assets/minimap/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29c928130d605c8c19c364a6a2e83bec.js"
+  "./precache-manifest.6c2c0f9caa9a5aacda45002010038b39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
