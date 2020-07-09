b493c768edb34a6b75a00f02e93ecc073d01817d
Nicolas Patry <patry.nicolas@protonmail.com>
2020-07-09 19:05:50 +0200

    Adding minimap.

---
+++ assets/minimap/precache-manifest.29c928130d605c8c19c364a6a2e83bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feca3edfc3288b592dcb279e7b64be19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e28b96d9584765cc53",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee50094b89b6a66bd17c",
+    "url": "/static/js/2.4eb4f33a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4eb4f33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e28b96d9584765cc53",
+    "url": "/static/js/main.483e8d67.chunk.js"
+  },
+  {
+    "revision": "e7339d1658efd2921338",
+    "url": "/static/js/runtime-main.61cde8aa.js"
+  }
+]);
