4fe40a98abe5610c21c6f5e8a67904c6fe8747b0
Nicolas Patry <patry.nicolas@protonmail.com>
2020-07-22 19:06:34 +0200

    Upgrade endpoint to translate.ottomate.app

--- assets/translate/precache-manifest.ceb8946f959e5af311421423288aabc6.js
+++ assets/translate/precache-manifest.ceb8946f959e5af311421423288aabc6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9cd2c380af3089b45f056fcf4b586e9",
+    "revision": "278307e68e050c2e13532cfe0fc6f334",
     "url": "./index.html"
   },
   {
-    "revision": "1821a80b87d660e62e6b",
+    "revision": "359f638a7f384a1d42a4",
     "url": "./static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.b3e1e689.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1821a80b87d660e62e6b",
-    "url": "./static/js/main.a2f9d418.chunk.js"
+    "revision": "359f638a7f384a1d42a4",
+    "url": "./static/js/main.e9582362.chunk.js"
   },
   {
     "revision": "7304c6cd1204852fb3fb",
